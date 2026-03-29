4b436c6097441aafe4cca9d82bcb9e30ee9f90ec
Het Sakariya <hetsakariya.0111@gmail.com>
2026-03-29 11:09:26 +0530

    Delete Node Course directory

--- Node Course/Node.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile("output.txt","Writing file", (err)=>{
-  if(err) console.log("Error occurred");
-  else console.log("File Written Successfully");
-})
-
-console.log("Het");  
