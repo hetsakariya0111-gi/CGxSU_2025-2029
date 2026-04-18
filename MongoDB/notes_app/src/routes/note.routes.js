ee3561dd9f5ecf3b9edd81c5899db3ad20d9ba26
hetsakariya2007 <het.sakariya.cg@gmail.com>
2026-04-18 08:47:04 +0530

    feat: add replace note endpoint (PUT /api/notes/:id)

--- MongoDB/notes_app/src/routes/note.routes.js
+++ MongoDB/notes_app/src/routes/note.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createBulkNotes, getAllNotes, getNoteById } = require("../controllers/note.controller");
+const { createNote, createBulkNotes, getAllNotes, getNoteById, replaceNote } = require("../controllers/note.controller");
 
 router.post("/bulk", createBulkNotes);
 router.post("/", createNote);
 router.get("/", getAllNotes);
 router.get("/:id", getNoteById);
+router.put("/:id", replaceNote);
 
 module.exports = router;
