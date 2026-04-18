5324d91caeadfabb1d5911b021b0a7a66bbc5ae5
hetsakariya2007 <het.sakariya.cg@gmail.com>
2026-04-18 08:36:23 +0530

    feat: add get all notes endpoint (GET /api/notes)

--- MongoDB/notes_app/src/controllers/note.controller.js
+++ MongoDB/notes_app/src/controllers/note.controller.js
@@ -56,4 +56,22 @@ const createBulkNotes = async (req, res) => {
   }
 };
 
-module.exports = { createNote, createBulkNotes };
+const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+
+    res.status(200).json({
+      success: true,
+      message: "Notes fetched successfully",
+      data: notes,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
+module.exports = { createNote, createBulkNotes, getAllNotes };
--- MongoDB/notes_app/src/routes/note.routes.js
+++ MongoDB/notes_app/src/routes/note.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createBulkNotes } = require("../controllers/note.controller");
+const { createNote, createBulkNotes, getAllNotes } = require("../controllers/note.controller");
 
 router.post("/bulk", createBulkNotes);
 router.post("/", createNote);
+router.get("/", getAllNotes);
 
 module.exports = router;
