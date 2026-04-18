fb6fbe62cdc58feedfd0ce24b4b6d6a64e799ba0
hetsakariya2007 <het.sakariya.cg@gmail.com>
2026-04-18 08:49:25 +0530

    feat: add update note endpoint (PATCH /api/notes/:id)

--- MongoDB/notes_app/src/routes/note.routes.js
+++ MongoDB/notes_app/src/routes/note.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, createBulkNotes, getAllNotes, getNoteById, replaceNote } = require("../controllers/note.controller");
+const { createNote, createBulkNotes, getAllNotes, getNoteById, replaceNote, updateNote } = require("../controllers/note.controller");
 
 router.post("/bulk", createBulkNotes);
 router.post("/", createNote);
 router.get("/", getAllNotes);
 router.get("/:id", getNoteById);
 router.put("/:id", replaceNote);
+router.patch("/:id", updateNote);
 
 module.exports = router;
